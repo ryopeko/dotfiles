095e5709b03093e34c9dd48ce1277034fa551f9b
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-06-06 01:11:26 +0900

    add ghq and percol setting

--- ryopeko.zsh
+++ ryopeko.zsh
@@ -75,3 +75,15 @@ if [ -n "${TMUX}" ]; then
   # ショートカットキー割り当て
   bindkey "^t" update_ssh_auth_sock
 fi
+
+
+function percol-src () {
+    local selected_dir=$(ghq list --full-path | percol --query "$LBUFFER")
+    if [ -n "$selected_dir" ]; then
+        BUFFER="cd ${selected_dir}"
+        zle accept-line
+    fi
+    zle clear-screen
+}
+zle -N percol-src
+bindkey '^S' percol-src
