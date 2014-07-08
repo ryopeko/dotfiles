dbca547dc25872a115f28412d4de283246a78b08
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-07-08 22:54:51 +0900

    add brew setup file

---
+++ brew_install.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+brew update
+
+brew tap peco/peco
+
+brew install vim --with-lua
+
+brew_packages=(
+zsh
+git
+hg
+tmux
+rbenv
+ruby-build
+go
+peco
+ag
+jq
+wget
+tree
+redis
+)
+
+cask_packages=(
+iterm2
+keyremap4macbook
+google-chrome
+google-japanese-ime
+firefox
+limechat
+istat-menus
+alfred
+slack
+dropbox
+bettertouchtool
+)
+
+for package in ${brew_packages[@]} ; do
+  echo "check $package"
+  brew list |grep $package
+  if [ $? -gt 0 ]; then
+     echo "install $package"
+     brew install $package
+  else
+     echo "update $package"
+     brew upgrade $package
+  fi
+done
+
+for package in ${cask_packages[@]}; do
+  echo "check $package"
+  brew cask list |grep $package
+  if [ $? -gt 0 ]; then
+     echo "install $package"
+     brew cask install $package --verbose
+  else
+     echo "installed $package"
+  fi
+done
--- setup.sh
+++ setup.sh
@@ -2,6 +2,8 @@
 
 DOT_HOME=$HOME/dotfiles
 
+bash $DOT_HOME/brew_install.sh
+
 cd $HOME
 git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
