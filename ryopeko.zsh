d4b6bb10da57c20229a84315decd73f76ab383a2
Ryoichi SEKIGUCHI <ryopeko+free@gmail.com>
2014-07-22 23:45:22 +0900

    externalize of peco settings

--- ryopeko.zsh
+++ ryopeko.zsh
@@ -77,36 +77,5 @@ if [ -n "${TMUX}" ]; then
   bindkey "^t" update_ssh_auth_sock
 fi
 
-
-function peco-src () {
-    local selected_dir=$(ghq list --full-path | peco --query "$LBUFFER")
-    if [ -n "$selected_dir" ]; then
-        BUFFER="cd ${selected_dir}"
-        zle accept-line
-    fi
-    zle clear-screen
-}
-zle -N peco-src
-bindkey '^S' peco-src
-
-function peco-github-prs () {
-    local pr=$(repo=`g config remote.origin.url |cut -d ":" -f 2 |sed -e s/\.git$//` | curl -s -H "Authorization: token $TOKEN" $GITHUB_API_ENDPOINT/repos/$repo/pulls |jq -r '.[] |"\(.html_url)\t\(.title)\t\(.user.login)"' | peco --query "$LBUFFER"|cut -f 1)
-    if [ -n "$pr" ]; then
-        BUFFER="open ${pr}"
-        zle accept-line
-    fi
-    zle clear-screen
-}
-zle -N peco-github-prs
-bindkey '^G^P' peco-github-prs
-
-function cdgem() {
-  local gem_name=$(bundle list | sed -e 's/^ *\* *//g' | peco | cut -d \  -f 1)
-  if [ -n "$gem_name" ]; then
-    local gem_dir=$(bundle show ${gem_name})
-    echo "cd to ${gem_dir}"
-    cd $gem_dir
-  fi
-}
-
+source $HOME/dotfiles/peco.zsh
 
